7a496761096df18dc57bf18c374c11bcc1c6d25b
Olya <124518552+olezhenk@users.noreply.github.com>
2026-06-03 17:21:35 +0000

    тест класса Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -1,4 +1,6 @@
-from main import Ingredient
+#pytest test_recipes.py
+import pytest
+from main import Ingredient, Recipe
 
 class TestIngredient:
     def test_init(self):
@@ -18,3 +20,39 @@ class TestIngredient:
         assert ingr1 == ingr2
         assert ingr1 != ingr3
 
+class TestRecipe:
+    def test_init(self):
+        ingr1 = Ingredient("Мука", 500, "г")
+        ingr2 = Ingredient("Яйца", 3, "шт")
+        recipe = Recipe("Блинчики", [ingr1, ingr2])
+        assert recipe.title == "Блинчики"
+        assert recipe.ingredients == [ingr1, ingr2]
+
+    def test_add_ingredient(self):
+        ingr1 = Ingredient("Молоко", 200, "мл")
+        ingr2 = Ingredient("Молоко", 100, "мл")
+        ingr3 = Ingredient("Сахар", 50, "г")
+        recipe = Recipe("Каша", [ingr1])
+        recipe.add_ingredient(ingr2)
+        assert recipe.ingredients[0].quantity == 300
+        recipe.add_ingredient(ingr3)
+
+    def test_scale(self):
+        ingr1 = Ingredient("Мука", 500, "г")
+        ingr2 = Ingredient("Яйца", 3, "шт")
+        recipe = Recipe("Блинчики", [ingr1, ingr2])
+        scaled_recipe = recipe.scale(2)
+        assert scaled_recipe.ingredients[0].quantity == 1000
+        assert scaled_recipe.ingredients[1].quantity == 6
+        with pytest.raises(ValueError):
+            recipe.scale(0)
+
+        with pytest.raises(ValueError):
+            recipe.scale(-1)
+
+    def test_len(self):
+        ingr1 = Ingredient("Мука", 500, "г")
+        ingr2 = Ingredient("Яйца", 3, "шт")
+        recipe = Recipe("Блинчики", [ingr1, ingr2])
+        assert len(recipe) == 2
+    
