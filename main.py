4cc21b30bf7c5fea69667cbfb4821233086c9258
Olya <124518552+olezhenk@users.noreply.github.com>
2026-06-03 16:11:00 +0000

    реализовала класс ShoppingList

--- main.py
+++ main.py
@@ -60,3 +60,38 @@ class Recipe:
 
     def __str__(self):
         return f"{self.title}: {self.ingredients}"
+
+class ShoppingList:
+    def __init__(self):
+        self._items = []
+
+    def add_recipe(self, recipe: Recipe, portions: float):
+      if portions <=0:
+         raise ValueError("Количество порций должно быть положительным")
+      else:
+         recipe.scale(portions)
+         for ingr in recipe.ingredients:
+            self._items.append((ingr, recipe.title))
+
+    def remove_recipe(self, recipe: Recipe):
+        for item in self._items:
+            if item[1] == recipe.title:
+                self._items.remove(item)
+
+    def get_list(self):
+        total_list = {}
+        for item in self._items:
+            ingr = item[0]
+            if ingr.name in total_list:
+                total_list[ingr.name] += ingr.quantity
+            else:
+                total_list[ingr.name] = ingr.quantity
+        return total_list
+    
+    def __add__(self, other):
+        new_list = ShoppingList()
+        for item in self._items:
+            new_list._items.append(item)
+        for item in other._items:
+            new_list._items.append(item)
+        return new_list
