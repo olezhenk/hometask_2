2fa90756cd0c07483fa7db9cf5861e513533f721
Olya <124518552+olezhenk@users.noreply.github.com>
2026-06-03 15:54:18 +0000

    реализован класс Recipe

--- main.py
+++ main.py
@@ -26,3 +26,37 @@ class Ingredient:
       return Ingredient(ingr1.name, ingr1.quantity + ingr2.quantity, ingr1.unit)
     else:
       return ingr1
+
+
+class Recipe:
+    def __init__(self, title, ingredients: list):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for ingr in self.ingredients:
+            if ingr.name == ingredient.name and ingr.unit == ingredient.unit:
+                ingr.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if ratio.isinstance(float):
+            if ratio <=0:
+                return False
+            else:
+                return True
+        else:
+            return False
+
+    def scale(self, ratio:float):
+        for ingr in self.ingredients:
+            ingr.quantity *= ratio
+        return self
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        return f"{self.title}: {self.ingredients}"
